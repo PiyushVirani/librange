da47f71b851df2683a14a11cbdfe15630abe3e83
PiyushVirani <146486681+PiyushVirani@users.noreply.github.com>
2025-12-25 16:50:11 -0500

    create draft of React Project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>HinLIBS Project</h1>
+      <p>Start editing components to see changes.</p>
+    </div>
+  );
+}
+
+export default App;
