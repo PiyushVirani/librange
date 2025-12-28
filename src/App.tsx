9de9fbf4c360a52ddccf7e60e6cd1949070ab112
PiyushVirani <146486681+PiyushVirani@users.noreply.github.com>
2025-12-27 21:49:34 -0500

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import {useState} from 'react';
 import CallNumberForm from './components/CallNumberForm';
 
-
 function App() {
+  const [currentSearch, setCurrentSearch] = useState{''};
+
+  const handleSearch = (term: string) => {
+    setCurrentSearch(term);
+    console.log("App received:", term);
+  };
   return (
     <div>
       <h1>LibRange Project</h1>
       <p>Enter a call number below to find its location.</p>
 
-    <CallNumberForm/>
+    <CallNumberForm onSubmit={handleSearch} />
+
+    <hr />
+
+    <h3>Current Search: {currentSearch} </h3>
 
     </div>
   );
